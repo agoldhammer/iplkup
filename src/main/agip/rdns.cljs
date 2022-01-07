ad19e6f97d71b926a63ea3d9c441b69a90838fad
Art Goldhammer <art.goldhammer@gmail.com>
2022-01-07 16:07:00 -0500

    confused

--- src/main/agip/rdns.cljs
+++ src/main/agip/rdns.cljs
@@ -2,58 +2,56 @@
   (:require ["fs" :as fs]
             ["dns" :as dns]
             [process :as p]
-          [clojure.string :as s]))
+            [clojure.string :as s]))
 
-(p/on "uncaughtException", (fn[err origin]
-                                   (println "Uncaught Exception" err origin)))
+(p/on "uncaughtException", (fn [err origin]
+                             (println "Uncaught Exception" err origin)))
 
 (defn slurp [file]
   (-> (fs/readFileSync file)
       (.toString)))
 
-#_(def dns-promises dns/promises)
 
 (defn rev-dns
   "do reverse dns lookup on vec of ips"
   [ips]
-(mapv dns/promises.reverse ips))
-
-
-#_(defn rev-dns2
-  "do reverse dns lookup on vec of ips"
-  [ips]
-  (mapv dns-promises.reverse ips))
+  (mapv #(dns/promises.reverse % "CNAME") ips))
 
 (def ips (s/split-lines (slurp "ips.txt")))
-(take 3 ips)
-(count ips)
 
-(def proms (rev-dns (take 15 ips)))
-(take 3 proms)
-(doseq [prom (take 15 proms)]
-  (.then prom #(println (js->clj %))))
+#_(def proms (rev-dns (take 6 ips)))
 
-(first proms)
-(count proms)
+(defn proc-proms
+  "print a sequence of dns reverse lookups of vec of ips"
+  [ips]
+  (doseq [prom (rev-dns ips)]
+    #_(println prom)
+    (-> prom
+        (.then #(js/console.log %))
+        (.catch #(js/console.log "lkup err" %)))))
 
-(.then (first proms) #(println (js->clj %)))
+#_(.then (first proms) #(println (js->clj %)))
 
 (defn -main
   [& args]
   (println "main" args))
 
 (comment
-(def prom (dns/promises.reverse "192.74.137.6" "CNAME"))
-  (.then prom #(println (js->clj %))))
+  (js/console.log "err" 2)
+  (def prom (dns/promises.reverse "192.74.137.6" "CNAME"))
+  (.then prom #(println (js->clj %))) println
+  (.then (dns/promises.reverse "51.79.29.48" println))
+  (proc-proms (take 5 ips))
+  )
 
 
-(comment 
-         (println "hi" 5)
-         (def s (slurp "ips.txt"))
-         (s/split-lines s)
-         (dns/lookup "example.org"
-                     (fn [err add fam]
-                       (println err add fam))))
+(comment
+  (println "hi" 5)
+  (def s (slurp "ips.txt"))
+  (s/split-lines s)
+  (dns/lookup "example.org"
+              (fn [err add fam]
+                (println err add fam))))
 
 
 
