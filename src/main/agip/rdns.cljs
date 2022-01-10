b91ec5797666810726f740aeca446931c1caebde
Art Goldhammer <art.goldhammer@gmail.com>
2022-01-10 14:16:06 -0500

    more cleanup

--- src/main/agip/rdns.cljs
+++ src/main/agip/rdns.cljs
@@ -12,6 +12,22 @@
 #_(def exit-chan (a/chan 1))
 (enable-console-print!)
 
+;; from David Nolen gist
+(defn timeout [ms]
+  (let [c (a/chan)]
+    (js/setTimeout (fn [] (a/close! c)) ms)
+    c))
+
+(comment
+  (a/go
+    (a/<! (timeout 1000))
+    (println "Hello")
+    (a/<! (timeout 1000))
+    (println "async")
+    (a/<! (timeout 1000))
+    (println "world!"))
+  )
+
 (defn slurp [file]
   (-> (fs/readFileSync file)
       (.toString)))
@@ -20,7 +36,7 @@
 (defn ip->host
   "reverse lkup ip"
   [ip]
-  (println "processing ip:" ip)
+  #_(println "processing ip:" ip)
   (dns/promises.reverse ip "CNAME"))
 
 (comment
@@ -37,7 +53,7 @@
   "pipeline function to revese lkup ip"
   [ip result-ch]
   (a/go [ip result-ch]
-        (println "async-process-ip processing ip " ip)
+        #_(println "async-process-ip processing ip " ip)
         (a/>! result-ch {:ip ip :promise (ip->host ip)})
         (a/close! result-ch)))
 
@@ -49,27 +65,40 @@
     (a/pipeline-async 8 out-ch async-process-ip (a/to-chan! ips))
     out-ch))
 
-
-(defn process-ips
-  "process a vector of ips"
-  [ips]
-  (let [out-ch (pipe-ips ips)]
-    (a/go-loop [item (a/<! out-ch)
-                acc []]
-      (println "process-ips loop")
-      (if item
-        (recur (a/<! out-ch) (into acc item))
-        #_(println "accum" acc)
-        (let [ips (mapv (comp first rest) (filter #(= :ip (get % 0)) acc))
-              proms (mapv (comp first rest) (filter #(= :promise (get % 0)) acc))
-              settled (js/Promise.allSettled proms)]
-          (println "in process-ips: nproms" (count proms))
-          (println "ips" ips)
+(defn make-host-channel
+  "create and return channel to receive outcome of host lookups"
+  []
+  (let [host-chan (a/chan 1)]
+    (a/go-loop [item (a/<! host-chan)]
+      (when item
+        (let [[ips settled] item]
+          (println  "host-chan count" (count ips))
+          #_(println settled)
           (->
            settled
            (.then #(js/console.log "output" %))
            (.catch #(js/console.log "error"))
-           (.finally #(println :finis))))))))
+           (.finally #(println :finis))))
+        
+        (recur (a/<! host-chan))))
+    host-chan))
+
+
+(defn process-ips
+  "process a vector of ips"
+  [ips]
+  (let [out-ch (pipe-ips ips)
+        host-chan (make-host-channel)]
+    (a/go-loop [item (a/<! out-ch)
+                acc []]
+      #_(println "process-ips loop")
+      (if item
+        (recur (a/<! out-ch) (into acc item))
+        (let [ips (mapv (comp first rest) (filter #(= :ip (get % 0)) acc))
+              proms (mapv (comp first rest) (filter #(= :promise (get % 0)) acc))
+              settled (js/Promise.allSettled proms)]
+          #_(println "in process-ips: nproms" (count proms))
+          (a/>! host-chan [ips settled]))))))
 
 (defn process-file
   "process a file of ips"
@@ -83,16 +112,15 @@
   (pr/on "exit" (fn [code] (js/console.log "exiting" code)))
   (println "Welcome" args)
   (process-file "ips.txt")
-  (js/setTimeout #(pr/exit 0) 1000)
+  (js/setTimeout #(pr/exit 0) 2000)
   #_(println "main" args))
 
 (comment
-  (do
-    (process-ips ["34.86.35.10" "52.41.81.117"])
-    (pr/on "exit" (fn [code] (js/console.log "exiting" code))))
+  (process-ips ["34.86.35.10" "52.41.81.117"])
   (-main "hi")
-(process-ips (take 5 (s/split-lines (slurp "ips.txt"))))
-
+  (process-ips (take 5 (s/split-lines (slurp "ips.txt"))))
+  (def ps (js/Promise.resolve (#(js/setTimeout (fn [] {:ans 42}))) 1000))
+  (.then ps println)
   )
 
 
