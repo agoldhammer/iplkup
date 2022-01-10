8880d082b4cd2a515264bda130b08ca0bbd3a49e
Art Goldhammer <art.goldhammer@gmail.com>
2022-01-10 11:23:10 -0500

    cleanup

--- src/main/agip/rdns.cljs
+++ src/main/agip/rdns.cljs
@@ -17,11 +17,6 @@
       (.toString)))
 
 
-#_(defn rev-dns
-    "do reverse dns lookup on vec of ips"
-    [ips]
-    (mapv #(dns/promises.reverse % "CNAME") ips))
-
 (defn ip->host
   "reverse lkup ip"
   [ip]
@@ -35,28 +30,8 @@
         (.then println println)
         (.catch println println)
         (.finally #(println "finis"))))
-  ;;;
-  #_(a/take! (a/go
-             (try
-               (<p! (ip->host "172.182.1.25"))
-               (catch js/Error err (js/console.log (ex-cause err))))) println)
-
-  ;;;
-  (comment
-    (def p (js/Promise.all (mapv ip->host ["34.86.35.10" #_"172.182.1.25"])))
-
-    (-> p
-        (.then println)
-        (.catch println))
-    )
-  (dns/reverse "34.86.35.10" (fn [err hns]
-                               (if err
-                                 (println err)
-                                 (println hns))))
-  (dns/reverse "172.182.1.25" (fn [err hns]
-                                (if err
-                                  (println "error")
-                                  (println hns)))))
+  
+  )
 
 (defn async-process-ip
   "pipeline function to revese lkup ip"
@@ -74,20 +49,6 @@
     (a/pipeline-async 8 out-ch async-process-ip (a/to-chan! ips))
     out-ch))
 
-#_(defn resolve-item
-  "resolve hostname from promise"
-  [item]
-  (a/go
-    (let [{:keys [ip promise]} item]
-      (println "resolve-item" item)
-      (println "resolve-item" ip promise)
-      #_(-> promise
-            (.then js/console.log)
-            (.catch js/console.log)
-            (.finally #(js/console.log "fin" ip)))
-      (try
-        (a/take! (<p! promise) println)
-        (catch js/Error err (js/console.log (ex-cause err)))))))
 
 (defn process-ips
   "process a vector of ips"
@@ -133,39 +94,6 @@
 (process-ips (take 5 (s/split-lines (slurp "ips.txt"))))
 
   )
-
-(comment
-  (js/console.log "err" 2)
-  (-> (js/Promise.reject (js/Error "fail"))
-      (.then #(println (.-Error %)))
-      (.catch #(js/console.log %))
-      #_(.catch #(js/console.log (ex-cause %)))
-      (.finally #(println "done")))
-  (def prom (dns/promises.reverse "192.74.137.6" "CNAME"))
-  (.then prom #(println (js->clj %)))
-  (.then (dns/promises.reverse "51.79.29.48" println))
-  )
-
-(comment
-  (def p (dns/promises.reverse "34.86.35.10" "CNAME"))
-  (.then p #(println (js->clj %)))
-  (println "hi" 5)
-  (def s (slurp "ips.txt"))
-  (s/split-lines s)
-  (dns/lookup "example.org"
-              (fn [err add fam]
-                (println err add fam)))
-  (js/setTimeout #(println "hi 2") 2000))
-
-
-(comment
-  (let
-   [ips (s/split-lines (slurp "ips.txt"))
-    c (a/chan 256)]
-    (count ips)
-    (ip->host "34.86.35.10")
-    (println "wait 1")
-    (js/setTimeout #(a/go (println (a/<! c))) 1000)))
 
 
 
